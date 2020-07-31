19c4dc2d37e25fa07144cc38e02be3f1ec47ceb3
清流 <ruoge3s@qq.com>
2020-07-31 16:50:04 +0800

    增加阿里云ACM配置读取功能

--- app/command/Ali.php
+++ app/command/Ali.php
@@ -5,6 +5,8 @@ namespace app\command;
 
 use core\Command;
 use core\Config;
+use GuzzleHttp\Client;
+use GuzzleHttp\Exception\GuzzleException;
 use OSS\Core\OssException;
 use OSS\OssClient;
 
@@ -46,9 +48,42 @@ class Ali extends Command
         }
     }
 
+    /**
+     * 获取阿里云ACM配置
+     * @throws GuzzleException
+     */
     public function acm()
     {
         $config = Config::instance()->get('ali.acm');
-        print_r($config);
+
+        // curl acm.aliyun.com:8080/diamond-server/diamond
+
+        $ip = '139.196.135.144';
+
+        $client = new Client([
+            'base_uri'  => "$ip:8080",
+            'timeout'   => 35
+        ]);
+
+        $ts = round(microtime(true) * 1000);
+
+        $signStr = "{$config['namespace']}+{$config['group']}+{$ts}";
+
+        $response = $client->get('/diamond-server/config.co', [
+            'headers'   => [
+                'Spas-AccessKey'    => $config['access_key'],
+                'timeStamp'         => $ts,
+                'Spas-Signature'    => base64_encode(hash_hmac('sha1', $signStr, $config['secret_key'],true)),
+            ],
+            'query'      => [
+                "tenant"    => $config['namespace'],
+                "dataId"    => $config['data_id'],
+                "group"     => $config['group'],
+            ]
+        ]);
+
+        $c = $response->getBody()->getContents();
+
+        echo iconv("GB2312//IGNORE", "UTF-8", $c);
     }
 }
